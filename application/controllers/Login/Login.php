9aa8eb9cdb69831904df26cb56576adcdb350442
vaidehi <vaidehi.bizmotech@gmail.com>
2018-03-06 12:43:37 +0530

    commit by vaidehi

---
+++ application/controllers/Login/Login.php
@@ -0,0 +1,12 @@
+<?php 
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+error_reporting(E_ERROR | E_PARSE);
+
+//Login controller
+class Login extends CI_Controller
+{
+	public function __construct(){
+		parent::__construct();
+		
+	}
+}
