8bc7689637b2ba2452e49cf2e2142f191a56a94a
being-samrat <mundesamrat@gmail.com>
2018-03-21 15:20:09 +0530

    cd

--- application/controllers/orders/Manage_orders.php
+++ application/controllers/orders/Manage_orders.php
@@ -21,97 +21,106 @@ class Manage_orders extends CI_controller{
     $this->load->library('user_agent');
         //$data['orders'] = Manage_orders::getMyOrders();
         //print_r($data);//-------show all Raw prods
-        $user_id = $this->session->userdata('user_id');
+    $user_id = $this->session->userdata('user_id');
 
-        $path = base_url();
-        $url = $path . 'api/ManageOrder_api/numRows?user_id='.$user_id;
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPGET, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response_data = curl_exec($ch);
-        curl_close($ch);
+    $path = base_url();
+    $url = $path . 'api/ManageOrder_api/numRows?user_id='.$user_id;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_data = curl_exec($ch);
+    curl_close($ch);
         //----------pagination code starts here-------------------------------------
         //------loading the library pagination----------------------//
-        $this->load->library('pagination');
+    $this->load->library('pagination');
         //--------------creating the config array for pagination basic requirements----------------//
-        $config = [
-            'base_url' => base_url('orders/Manage_orders/index'),
-            'per_page' => 10,
-            'total_rows' => json_decode($response_data, true),
-        ];
-        $config['full_tag_open'] = "<ul class='pagination' style='color:black'>";
-        $config['full_tag_close'] = '</ul>';
-        $config['num_tag_open'] = '<li style="color:black">';
-        $config['num_tag_close'] = '</li>';
-        $config['cur_tag_open'] = '<li class="active" style="background-color: #4CAF50;"><a href="#" style="color:white">';
-        $config['cur_tag_close'] = '</a></li>';
-        $config['prev_tag_open'] = '<li style="color:black">';
-        $config['prev_tag_close'] = '</li>';
-        $config['first_tag_open'] = '<li style="color:black">';
-        $config['first_tag_close'] = '</li>';
-        $config['last_tag_open'] = '<li style="color:black">';
-        $config['last_tag_close'] = '</li>';
+    $config = [
+      'base_url' => base_url('orders/Manage_orders/index'),
+      'per_page' => 10,
+      'total_rows' => json_decode($response_data, true),
+    ];
+    $config['full_tag_open'] = "<ul class='pagination' style='color:black'>";
+    $config['full_tag_close'] = '</ul>';
+    $config['num_tag_open'] = '<li style="color:black">';
+    $config['num_tag_close'] = '</li>';
+    $config['cur_tag_open'] = '<li class="active" style="background-color: #4CAF50;"><a href="#" style="color:white">';
+    $config['cur_tag_close'] = '</a></li>';
+    $config['prev_tag_open'] = '<li style="color:black">';
+    $config['prev_tag_close'] = '</li>';
+    $config['first_tag_open'] = '<li style="color:black">';
+    $config['first_tag_close'] = '</li>';
+    $config['last_tag_open'] = '<li style="color:black">';
+    $config['last_tag_close'] = '</li>';
 
-        $config['prev_link'] = '<i class="fa fa-long-arrow-left" ></i> Previous';
-        $config['prev_tag_open'] = '<li style="color:black">';
-        $config['prev_tag_close'] = '</li>';
+    $config['prev_link'] = '<i class="fa fa-long-arrow-left" ></i> Previous';
+    $config['prev_tag_open'] = '<li style="color:black">';
+    $config['prev_tag_close'] = '</li>';
 
-        $config['next_link'] = 'Next <i class="fa fa-long-arrow-right" ></i>';
-        $config['next_tag_open'] = '<li style="color:black">';
-        $config['next_tag_close'] = '</li>';
+    $config['next_link'] = 'Next <i class="fa fa-long-arrow-right" ></i>';
+    $config['next_tag_open'] = '<li style="color:black">';
+    $config['next_tag_close'] = '</li>';
         //-----initialise pagination library with passing parameter config-----------//
-        $this->pagination->initialize($config);
+    $this->pagination->initialize($config);
         //-----initialise pagination library with passing parameter config-----------//
-        $data["links"] = $this->pagination->create_links();
-        $path = base_url();
-        $url = $path . 'api/ManageOrder_api/getMyOrders?user_id='.$user_id.'&per_page='.$config['per_page'].'&offset='.$this->uri->segment(4);
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPGET, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response_json = curl_exec($ch);
-        curl_close($ch);
-        $data['orders'] = json_decode($response_json, true);
+    $data["links"] = $this->pagination->create_links();
+    $path = base_url();
+    $url = $path . 'api/ManageOrder_api/getMyOrders?user_id='.$user_id.'&per_page='.$config['per_page'].'&offset='.$this->uri->segment(4);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_json = curl_exec($ch);
+    curl_close($ch);
+    $data['orders'] = json_decode($response_json, true);
 
-        if ($this->agent->is_mobile()) {
-            $this->load->view('includes/mobile/header');
-            $this->load->view('pages/orders/mobile/manage_orders', $data);
-            $this->load->view('includes/mobile/footer');
-        } else {
-            $this->load->view('includes/header');
-            $this->load->view('pages/orders/manage_orders', $data);
-        }
+    if ($this->agent->is_mobile()) {
+      $this->load->view('includes/mobile/header');
+      $this->load->view('pages/orders/mobile/manage_orders', $data);
+      $this->load->view('includes/mobile/footer');
+    } else {
+      $this->load->view('includes/header');
+      $this->load->view('pages/orders/manage_orders', $data);
     }
+  }
 
  //----------this function to get all my orders details-----------------------------
- public function getMyOrders() {
-  $user_id=$this->session->userdata('user_id');
+  public function getMyOrders() {
+    $user_id=$this->session->userdata('user_id');
   //$user_id=1;
 
-  $path = base_url();
-  $url = $path.'api/ManageOrder_api/getMyOrders?user_id='.$user_id;
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_HTTPGET, true);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $response_json = curl_exec($ch);
-  curl_close($ch);
-  $response = json_decode($response_json, true);
-  return $response;
-}
+    $path = base_url();
+    $url = $path.'api/ManageOrder_api/getMyOrders?user_id='.$user_id;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_json = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response_json, true);
+    return $response;
+  }
 //----------------this fun get all my orders details end---------------//
 
 
  //----------this function to add order profile-----------------------------//
-public function addOrder() { 
-  extract($_POST);
+  public function addOrder() { 
+    extract($_POST);
   //print_r($_POST);die();
   //print_r($_FILES);die();
 
-  if(!isset($prod_Name)){
-   echo '<h4 class="w3-text-red w3-medium"><i class="fa fa-warning"></i> Add at least one product.</h4>';
-   die();
- }
-  if($business_field == 0){
-   echo '<h4 class="w3-text-red w3-medium"><i class="fa fa-warning"></i> Select Business field first.</h4>';
+    if(!isset($prod_Name)){
+     echo '<h4 class="w3-text-red w3-medium"><i class="fa fa-warning"></i> Add at least one product.</h4>';
+     die();
+   }
+   if($business_field == 0){
+     echo '<h4 class="w3-text-red w3-medium"><i class="fa fa-warning"></i> Select Business field first.</h4>
+     <script>
+     if(navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {           
+      $("html").animate({scrollTop:0},"slow");
+    } else {
+     $("html,body").animate({scrollTop:0},"slow");
+   }
+   $("#business_field").css({"border-color":\'red\'});
+   </script>
+   ';
    die();
  }
  $user_id=$this->session->userdata('user_id');
