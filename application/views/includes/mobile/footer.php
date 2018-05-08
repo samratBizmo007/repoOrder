0b760b15b9354e3ec601ca8c7e531fa4057dad67
being-samrat <mundesamrat@gmail.com>
2018-05-08 11:52:20 +0530

    commit: mobile header n footer

--- application/views/includes/mobile/footer.php
+++ application/views/includes/mobile/footer.php
@@ -1,21 +1,39 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); 
-error_reporting(E_ERROR | E_PARSE);
+//error_reporting(E_ERROR | E_PARSE);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>css/font awesome/font-awesome.min.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>css/font awesome/font-awesome.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>css/w3.css">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>css/alert/jquery-confirm.css">
   <script type="text/javascript" src="<?php echo base_url(); ?>css/bootstrap/jquery-3.1.1.js"></script>
   <script type="text/javascript" src="<?php echo base_url(); ?>css/bootstrap/bootstrap.min.js"></script>
   <script type="text/javascript" src="<?php echo base_url(); ?>css/alert/jquery-confirm.js"></script>
 </head>
 <body class="">
-    <div class="w3-small w3-bottom" style=" position: fixed;z-index: 3">
+  <div class="w3-row w3-center w3-border-top header_bg w3-bottom w3-border-top" style="position: fixed;z-index: 3">
+    <div class="w3-col s2 w3-center" style="padding:0 ">
+      <a href="<?php echo base_url(); ?>user/dashboard" class="btn icon"><label><i class="fa fa-home icon_active w3-xlarge "></i></label>
+      </a>
+    </div>
+    <div class="w3-col s2 w3-center" style="padding:0 0 0 12px">
+      <a href="<?php echo base_url(); ?>user/dashboard" class="btn icon"><label><i class="fa fa-search w3-text-lightgrey w3-xlarge"></i></label>
+      </a>
+    </div>
+    <div class="w3-col s4 w3-center" style="padding:0 ">
+      <a href="<?php echo base_url(); ?>user/dashboard" class="btn icon"><label><i class="fa fa-plus-square w3-text-lightgrey w3-xlarge"></i></label>
+      </a>
+    </div>
+    <div class="w3-col s2 w3-center">
+      <a href="<?php echo base_url(); ?>user/dashboard" class="btn icon"><label><i class="fa fa-cubes w3-text-lightgrey w3-xlarge"></i></label>
+      </a>
+    </div>
+    <div class="w3-col s2 w3-center" style="padding:0 ">
+      <a href="<?php echo base_url(); ?>user/dashboard" class="btn icon"><label><i class="fa fa-user w3-text-lightgrey w3-xlarge"></i></label>
+      </a>
+    </div>
+  </div>
+  
+    <!-- <div class="w3-small w3-bottom" style=" position: fixed;z-index: 3">
     <?php
     $last = $this->uri->total_segments();
     $record_num = $this->uri->segment($last);
@@ -33,6 +51,6 @@ error_reporting(E_ERROR | E_PARSE);
       <a href="<?php echo base_url(); ?>user/user_settings" style="width: 100%;height: 100%;padding-left:10px;background-color: <?php if($record_num=='user_settings'){ echo '#0097A7'; }else{ echo '#bcb8b8'; } ?>" class="w3-button w3-text-white"><label>Settings</label></a>
     </div>
 
-  </div>
+  </div> -->
 </body>
 </html>
--- application/views/includes/mobile/header.php
+++ application/views/includes/mobile/header.php
@@ -3,23 +3,20 @@
 <!DOCTYPE html>
 <html>
 <head>
- 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap/bootstrap.min.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/font awesome/font-awesome.min.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/font awesome/font-awesome.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/w3.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/alert/jquery-confirm.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/header/header_overlay.css">
-<script type="text/javascript" src="<?php echo base_url(); ?>css/bootstrap/jquery-3.1.1.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>css/bootstrap/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>css/alert/jquery-confirm.js"></script>
-<style></style>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/font awesome/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/font awesome/font-awesome.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/w3.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/alert/jquery-confirm.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/header/header_overlay.css">
+	<style></style>
 </head>
 <body>
-  <div class="w3-row w3-text-white w3-padding-left" style="background-color: #00B8D4">
-      <h5><b>Jumla Business</b></h5>
-    </div>
+	<div class="w3-row w3-center w3-border-bottom header_bg">
+		<h4>Jumla Business</h4>
+	</div>
 </body>
 </html>
