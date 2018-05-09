085f00c7d2439afc04035a503c598638eeb98c75
swapnilbirajdar09 <swapnil.bizmotech@gmail.com>
2018-05-09 10:46:34 +0530

    commit by swap

--- application/views/pages/user/mobile/profile/mobileedit_profile.php
+++ application/views/pages/user/mobile/profile/mobileedit_profile.php
@@ -13,17 +13,20 @@ error_reporting(E_ERROR | E_PARSE);
     <body class="">
         <!-- !PAGE CONTENT! -->
         <div class="w3-main" style="margin-top: 40px; margin-bottom: 40px;">
-            <!--            <header class="w3-container" >
-                            <h5><b><i class="fa fa-cog"></i> Edit Profile</b><a class="w3-button w3-right" href="<?php echo base_url(); ?>admin/user_profile"><span class="w3-text-orange"><b>Back To Profile </b></span></a></h5>
-                        </header>-->
-<?php //echo base64_decode('c3dhcDEyMzQ=');?>
             <div class="w3-col l9 w3-border-left w3-margin-bottom" id="editprofile">
                 <form id="editProfileForm" name="editProfileForm">
                     <div class="w3-col l12 w3-right">
                         <div class="col-sm-3 col-xs-3"></div>
                         <div class="w3-col s6 w3-center w3-padding">
                             <div class="text-center w3-padding profile_portfolio">
-                            <div class="w3-circle w3-center" title="profile image" style="margin-left: 7px; background-position:center; background-size:contain; background-image: url('<?php echo base_url() . $userDetails['status_message'][0]['user_image']; ?>'); height:80px;width:80px;"></div>
+                                <?php
+                                $default_image = 'images/default_male.png';
+                                if ($userDetails['status_message'][0]['user_image'] == '') {
+                                    ?>
+                                    <div class="w3-circle w3-center" title="profile image" style="margin-left: 7px; background-position:center; background-size:contain; background-image: url('<?php echo base_url() . $default_image; ?>'); height:80px;width:80px;"></div>
+                                <?php } else { ?>
+                                    <div class="w3-circle w3-center" title="profile image" style="margin-left: 7px; background-position:center; background-size:contain; background-image: url('<?php echo base_url() . $userDetails['status_message'][0]['user_image']; ?>'); height:80px;width:80px;"></div>
+                                <?php } ?>
 <!--                                <img class="img img-circle w3-center" alt="Profile Image not found" style="height: 50%; width: 50%; object-fit:contain; "  src="<?php echo base_url() . $userDetails['status_message'][0]['user_image']; ?>" id="profile_imagePreview" onerror="this.src='<?php echo base_url() ?>css/logos/default_noimage.jpg'">-->
                                 <div class="w3-col l12 ">
                                     <h6 class="w3-small" style="color: #00B8D4;">Change photo</h6>
@@ -90,7 +93,7 @@ error_reporting(E_ERROR | E_PARSE);
             <div class="w3-col l9 w3-border-left w3-margin-bottom" id="editprofile">
                 <hr>
                 <div class="w3-col l12 w3-center">
-                   <span class="w3-center w3-label w3-large">Change Password</span>
+                    <span class="w3-center w3-label w3-large">Change Password</span>
                 </div>
                 <form id="changepass_Form" name="changepass_Form">
                     <div class="w3-col l12 w3-padding w3-margin-top">
@@ -148,36 +151,36 @@ error_reporting(E_ERROR | E_PARSE);
             });
         });
         //-------------------fucntion to check confirm password---------------
-    function checkPassword() {
-        if ($('#new_password').val() == $('#conf_password').val()) {
-            $('#changepass_submit').prop("disabled", false);
-            $('#message').html('');
+        function checkPassword() {
+            if ($('#new_password').val() == $('#conf_password').val()) {
+                $('#changepass_submit').prop("disabled", false);
+                $('#message').html('');
 
-        } else {
-            $('#message').html('<label>Password Not Matching</label>').css('color', 'red');
-            $('#changepass_submit').prop("disabled", true);
+            } else {
+                $('#message').html('<label>Password Not Matching</label>').css('color', 'red');
+                $('#changepass_submit').prop("disabled", true);
+            }
         }
-    }
 //-----------function ends------------------------
 
 //  ------------------------Change Password -------------------------//
-    $(function () {
-        $("#changepass_Form").submit(function () {
-            dataString = $("#changepass_Form").serialize();
+        $(function () {
+            $("#changepass_Form").submit(function () {
+                dataString = $("#changepass_Form").serialize();
 
-            $.ajax({
-                type: "POST",
-                url: "<?php echo base_url(); ?>user/edit_profile/changePassword",
-                data: dataString,
-                return: false, //stop the actual form post !important!
-                success: function (data)
-                {
-                    $.alert(data);
-                }
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo base_url(); ?>user/edit_profile/changePassword",
+                    data: dataString,
+                    return: false, //stop the actual form post !important!
+                    success: function (data)
+                    {
+                        $.alert(data);
+                    }
+                });
+                return false;  //stop the actual form post !important!
             });
-            return false;  //stop the actual form post !important!
         });
-    });
 //  -------------------------END -------------------------------//
 
     </script>
