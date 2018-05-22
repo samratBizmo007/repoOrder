7041f3fe8dee19b40b6d24295b93a15d3cc1f5c3
being-samrat <mundesamrat@gmail.com>
2018-05-22 12:45:24 +0530

    commit: upload files

--- upload/prodImage_uploader.php
+++ upload/prodImage_uploader.php
@@ -2,7 +2,7 @@
 
 
      if (is_uploaded_file($_FILES['bill']['tmp_name'])) {
-    $uploads_dir = '../images/order_images/';
+    $uploads_dir = '../images//';
                             $tmp_name = $_FILES['bill']['tmp_name'];
                             $pic_name = $_FILES['bill']['name'];
                             move_uploaded_file($tmp_name, $uploads_dir.$pic_name);
